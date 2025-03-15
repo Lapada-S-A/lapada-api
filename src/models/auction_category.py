5a0885b9c1ccd23d142c3d4b07a5fecf10d1e8ca
MatheusSousa5068 <matheus5068trab@gmail.com>
2025-03-15 11:00:32 -0300

    fix: renaming fk

--- src/models/auction_category.py
+++ src/models/auction_category.py
@@ -2,6 +2,6 @@ from db import db
 
 auction_category = db.Table(
     'AuctionCategory',
-    db.Column('Auction_id', db.Integer, db.ForeignKey('Auction.id'), primary_key=True),
-    db.Column('Category_id', db.Integer, db.ForeignKey('Category.id'), primary_key=True)
+    db.Column('auction_id', db.Integer, db.ForeignKey('Auction.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('Category.id'), primary_key=True)
 )
